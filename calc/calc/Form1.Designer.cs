34ce3ed03d52f2b8b1f13629f07ec92e4af55fb0
nikito96 <33359937+nikito96@users.noreply.github.com>
2017-11-08 22:01:21 +0200

    Add files via upload

---
+++ calc/calc/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.sumButton = new System.Windows.Forms.Button();
+            this.subButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.multiButton = new System.Windows.Forms.Button();
+            this.divButton1 = new System.Windows.Forms.Button();
+            this.divButton2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(163, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // sumButton
+            // 
+            this.sumButton.Location = new System.Drawing.Point(23, 107);
+            this.sumButton.Name = "sumButton";
+            this.sumButton.Size = new System.Drawing.Size(75, 23);
+            this.sumButton.TabIndex = 2;
+            this.sumButton.Text = "+";
+            this.sumButton.UseVisualStyleBackColor = true;
+            this.sumButton.Click += new System.EventHandler(this.sumButton_Click);
+            // 
+            // subButton
+            // 
+            this.subButton.Location = new System.Drawing.Point(176, 107);
+            this.subButton.Name = "subButton";
+            this.subButton.Size = new System.Drawing.Size(75, 23);
+            this.subButton.TabIndex = 3;
+            this.subButton.Text = "-";
+            this.subButton.UseVisualStyleBackColor = true;
+            this.subButton.Click += new System.EventHandler(this.subButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "a";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(209, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "b";
+            // 
+            // multiButton
+            // 
+            this.multiButton.Location = new System.Drawing.Point(23, 176);
+            this.multiButton.Name = "multiButton";
+            this.multiButton.Size = new System.Drawing.Size(75, 23);
+            this.multiButton.TabIndex = 6;
+            this.multiButton.Text = "*";
+            this.multiButton.UseVisualStyleBackColor = true;
+            this.multiButton.Click += new System.EventHandler(this.multiButton_Click);
+            // 
+            // divButton1
+            // 
+            this.divButton1.Location = new System.Drawing.Point(176, 176);
+            this.divButton1.Name = "divButton1";
+            this.divButton1.Size = new System.Drawing.Size(33, 23);
+            this.divButton1.TabIndex = 7;
+            this.divButton1.Text = "/";
+            this.divButton1.UseVisualStyleBackColor = true;
+            this.divButton1.Click += new System.EventHandler(this.divButton1_Click);
+            // 
+            // divButton2
+            // 
+            this.divButton2.Location = new System.Drawing.Point(215, 176);
+            this.divButton2.Name = "divButton2";
+            this.divButton2.Size = new System.Drawing.Size(36, 23);
+            this.divButton2.TabIndex = 8;
+            this.divButton2.Text = "%";
+            this.divButton2.UseVisualStyleBackColor = true;
+            this.divButton2.Click += new System.EventHandler(this.divButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 241);
+            this.Controls.Add(this.divButton2);
+            this.Controls.Add(this.divButton1);
+            this.Controls.Add(this.multiButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.subButton);
+            this.Controls.Add(this.sumButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Calc";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button sumButton;
+        private System.Windows.Forms.Button subButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button multiButton;
+        private System.Windows.Forms.Button divButton1;
+        private System.Windows.Forms.Button divButton2;
+    }
+}
+
